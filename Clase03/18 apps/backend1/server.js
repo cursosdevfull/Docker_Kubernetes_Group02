8a716eea8a1ebfcd5b31a18b460d14e9a07171aa
sergio <sergiohidalgocaceres@gmail.com>
2021-02-07 02:50:28 -0500

    Clase03

---
+++ Clase03/18 apps/backend1/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const app = express();
+
+app.use(cors());
+app.get("/api/message", (req, res) => {
+  const info = {
+    message1: "Mensaje desde el backend1",
+  };
+
+  const endpointBackend2 = "http://localhost:3020";
+  axios.get(endpointBackend2 + "/api/message").then((response) => {
+    info.message2 = response.data.message;
+
+    res.json(info);
+  });
+});
+
+app.listen(3010, () => console.log("Backend1 en el puerto 3010"));
